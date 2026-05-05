cc9aace09623299088fdaf728875f129dc0d5faa
rColeJnr <ricardo.m.jnr@gmail.com>
2026-05-05 21:17:11 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "services"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,11 @@
+package com.productivesocial
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
